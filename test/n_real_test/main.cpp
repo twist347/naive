e09bed5fd0a138450b62071b7d644016daa676c8
Selimkhanov Emirkhan <EV_Selimkhanov@BNIPI.rosneft.ru>
2023-11-11 16:16:23 +0300

    add test for naive_real

--- test/n_real_test/main.cpp
+++ test/n_real_test/main.cpp
@@ -1,9 +1,9 @@
 #include <gtest/gtest.h>
-#include "n_array.h"
+#include "n_real.h"
 
-TEST(TestNaiveArray, SimpleTest) {
-    naive::array<int> arr = {1, 2, 3};
-    ASSERT_TRUE(arr[1] == 2);
+TEST(TestNaiveReal, SimpleTest) {
+    naive::real r{3.14};
+    ASSERT_DOUBLE_EQ(2 * r, 6.28);
 }
 
 int main(int argc, char **argv) {
